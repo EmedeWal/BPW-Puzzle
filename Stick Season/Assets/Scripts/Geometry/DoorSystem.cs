134dd15d5e29b528b3bdf6048ffef75c70978d7b
Eme de Wal <emewillemdewal@gmail.com>
2024-02-13 11:59:39 +0100

    Commit functioning closing/opening door system

--- Stick Season/Assets/Scripts/Geometry/DoorSystem.cs
+++ Stick Season/Assets/Scripts/Geometry/DoorSystem.cs
@@ -3,38 +3,79 @@ using System.Collections;
 
 public class DoorSystem : MonoBehaviour
 {
-    [SerializeField] private Transform movingDoor;
-
-    [SerializeField] private float maxDistance;
-    [SerializeField] private float moveSpeed;
-
+    private float offset = 10f;
+    private float moveDelay = 0.01f;
     private float moveAmount = 0.1f;
 
-   
+    private Coroutine currentCoroutine;
+
+    private Vector3 defaultPos;
+    private Vector3 newPos;
+
+    private void Awake()
+    {
+        // Set the defaultPos to the current position of the gameObject in the scene. 
+        defaultPos = base.transform.position;
+        Debug.Log(defaultPos);
+    }
+
+    private void Start()
+    {
+        // Set the newPos equal to defaultPos + the offset on the yAxis.
+        newPos = defaultPos + new Vector3(0, offset, 0);
+        Debug.Log(newPos);
+    }
 
     private void Update()
     {
        if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Open door is called");
-            StartCoroutine(OpenDoor());
+
+            // Check if there is a current Coroutine. If there is one active, stop it.
+            if (currentCoroutine != null) StopCoroutine(currentCoroutine);
+
+            // Start the correct Coroutine and assign it to currentCoroutine.
+            currentCoroutine = StartCoroutine(OpenDoor());
+        }
+
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Close the door.");
+
+            // Check if there is a current Coroutine. If there is one active, stop it.
+            if (currentCoroutine != null) StopCoroutine(currentCoroutine);
+
+            // Start the correct Coroutine and assign it to currentCoroutine.
+            currentCoroutine = StartCoroutine(CloseDoor());
         }
     }
 
     private IEnumerator OpenDoor()
     {
-        float distance = 0;
-
-        while (distance <= maxDistance)
+        // While the gameObject is lower than the desired position, move it up.
+        while (transform.position.y < newPos.y)
         {
-            Debug.Log("One loop of the function");
+            // Move the door upwards by the increment of moveAmount.
+            Vector3 newPosition = transform.position + Vector3.up * moveAmount;
+            transform.position = newPosition;
 
-            Vector3 newPosition = movingDoor.position + Vector3.up * moveAmount;
-            movingDoor.position = newPosition;
+            // Wait seconds equal to the moveDelay. Then, repeat.
+            yield return new WaitForSeconds(moveDelay);
+        }
+    }
 
-            distance += moveAmount;
+    private IEnumerator CloseDoor()
+    {
+        // While the gameObject is higher than the desired position, move it down.
+        while (transform.position.y > defaultPos.y)
+        {
+            // Move the door downwards by the increment of moveAmount.
+            Vector3 newPosition = transform.position + Vector3.down * moveAmount;
+            transform.position = newPosition;
 
-            yield return new WaitForSeconds(moveSpeed);
+            // Wait seconds equal to the moveDelay. Then, repeat.
+            yield return new WaitForSeconds(moveDelay);
         }
     }
 }
--- Stick Season/Assets/Scripts/Geometry/PressurePlate.cs
+++ Stick Season/Assets/Scripts/Geometry/PressurePlate.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    [SerializeField] private string tag;
+    [SerializeField] private string pressureTag;
+    [SerializeField] private GameObject door; 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(tag))
         {
-            Debug.Log("Player is standing on the pressure plate.");
+            Debug.Log("Player standing on the pressure plate");
         }
     }
 
